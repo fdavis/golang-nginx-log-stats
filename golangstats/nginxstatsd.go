ff88bd486223662ede68299e925cf833a5175fee
fletcher.davis <fletcher.davis@jibjab.com>
2016-03-21 22:24:10 -0700

    refactor, cleanup, define more consts
    pass filehandle instead of filename,
    reduces logic to handle keeping track of file seek location
    
    add more temp files to gitignore
    remote stats.log temp file
    
    move regex and max line length to consts on top of file

--- golangstats/nginxstatsd.go
+++ golangstats/nginxstatsd.go
@@ -15,13 +15,15 @@ import (
 const (
 	KEY_STATUS_CODE  = "status_code"
 	KEY_STATUS_ROUTE = "request_route"
+	PARSE_REGEX      = `[a-zA-Z0-9 /:,.+\[\]-]+"[A-Z]+ ([a-z0-9()+,\-.:=@;$_!*'%/?#]+) HTTP/1.[01]" ([0-9]{3})`
+	MAX_LINE_LENGTH  = 4 * 1024
 )
 
 var debug bool = false
 
 func debugOut(s string) {
 	if debug {
-		fmt.Println(s)
+		fmt.Printf(s)
 	}
 }
 
@@ -74,7 +76,7 @@ func ParseLine(line string) (map[string]string, error) {
 	ret := make(map[string]string)
 	// regex throws away first part of log until the request and ignores everything past status code
 	// save request route and status code
-	regex := regexp.MustCompile(`[a-zA-Z0-9 /:,.+\[\]-]+"[A-Z]+ ([a-z0-9()+,\-.:=@;$_!*'%/?#]+) HTTP/1.[01]" ([0-9]{3})`)
+	regex := regexp.MustCompile(PARSE_REGEX)
 	matches := regex.FindStringSubmatch(line)
 	if matches == nil {
 		debugOut(fmt.Sprintf("Could not match log line: %s\n", line))
@@ -86,24 +88,19 @@ func ParseLine(line string) (map[string]string, error) {
 }
 
 // read to end of nginx log file and return the offset
-func parseLogs(logFile string, stats *HttpStats, logPosition int64) int64 {
-	f, err := os.Open(logFile)
-	check(err)
-	defer f.Close()
+func parseLogs(f *os.File, stats *HttpStats) {
+	r := bufio.NewReaderSize(f, MAX_LINE_LENGTH)
 
-	f.Seek(logPosition, os.SEEK_SET)
-	r := bufio.NewReaderSize(f, 4*1024)
 	line, isPrefix, err := r.ReadLine()
-	// todo test moving update into for loop
 	for err == nil && !isPrefix {
 		s := string(line)
-		var result map[string]string
-		var parseErr error
-		result, parseErr = ParseLine(s)
+		result, parseErr := ParseLine(s)
 		if parseErr == nil {
-			debugOut(fmt.Sprintln(result))
+			debugOut(fmt.Sprintf("%v\n", result))
 			stats.update(result)
-			debugOut(fmt.Sprintln(stats.showStats()))
+			debugOut(fmt.Sprintf("%v", stats.showStats()))
+		} else {
+			fmt.Println(parseErr)
 		}
 		line, isPrefix, err = r.ReadLine()
 	}
@@ -114,17 +111,17 @@ func parseLogs(logFile string, stats *HttpStats, logPosition int64) int64 {
 	if err != io.EOF {
 		panic(err)
 	}
-	ret, err := f.Seek(logPosition, os.SEEK_SET)
-	check(err)
-	return ret
 }
 
 func main() {
 	poll := flag.Bool("poll", true, "if set keep running in the foreground else parse once and quit")
 	flag.BoolVar(&debug, "debug", false, "if set log more verboseley")
-	inputLogFilename := flag.String("inputLogFilename", "/var/log/nginx/access.log", "default access.log file to parse: /var/log/nginx/access.log")
-	// outputLogFilename := flag.String("outputLogFilename", "nginxstats.log", "default log destination file: nginxstats.log")
-	statsFilename := flag.String("statsFilename", "/var/log/stats.log", "default stats.log to write to: /var/log/stats.log")
+	inputLogFilename := flag.String("inputLogFilename", "/var/log/nginx/access.log",
+		"default access.log file to parse: /var/log/nginx/access.log")
+	// outputLogFilename := flag.String("outputLogFilename", "nginxstats.log",
+	//	"default log destination file: nginxstats.log")
+	statsFilename := flag.String("statsFilename", "/var/log/stats.log",
+		"default stats.log to write to: /var/log/stats.log")
 
 	flag.Parse()
 
@@ -138,11 +135,12 @@ func main() {
 	if *poll {
 		ticker := time.NewTicker(time.Second * 5)
 		go func() {
-			var logPosition int64 = 0
-			// todo, should open source log file here, otherwise if log rotates we will seek past the end of file
+			f, err := os.Open(*inputLogFilename)
+			check(err)
+			defer f.Close()
 			for t := range ticker.C {
-				debugOut(fmt.Sprintln("Tick at", t))
-				logPosition = parseLogs(*inputLogFilename, &myStats, logPosition)
+				debugOut(fmt.Sprintf("Tick at: %v", t))
+				parseLogs(f, &myStats)
 				statsFile.WriteString(myStats.showStats())
 				myStats.clear()
 			}
@@ -150,7 +148,10 @@ func main() {
 		// wait forever
 		select {}
 	} else {
-		parseLogs(*inputLogFilename, &myStats, 0)
+		f, err := os.Open(*inputLogFilename)
+		check(err)
+		defer f.Close()
+		parseLogs(f, &myStats)
 		debugOut(fmt.Sprintf(myStats.showStats()))
 		statsFile.WriteString(myStats.showStats())
 	}
